e507c18cfab47d0156d0e1e5d7bc58cc23617aaf
Jules Prevost <jprevostdev@gmail.com>
2020-06-15 16:39:43 +0200

    cloudinary config for development done

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'devise'
 
 gem 'dotenv-rails', groups: [:development, :test]
 
+gem 'cloudinary', '~> 1.12.0'
+
 group :development, :test do
   gem 'pry-byebug'
   gem 'pry-rails'
--- app/models/friend.rb
+++ app/models/friend.rb
@@ -3,6 +3,8 @@ class Friend < ApplicationRecord
   has_many :bookings
   has_many :users,  through: bookings
 
+  has_one_attached :photo
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
