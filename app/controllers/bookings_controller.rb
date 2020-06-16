c66626c332a924e03193a9609267343377ace6bd
Jules Prevost <jprevostdev@gmail.com>
2020-06-16 16:14:10 +0200

    finish update, edit and destroy method

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -19,14 +19,19 @@ class BookingsController < ApplicationController
   end
 
   def edit
+    @booking = Booking.find(params[:id])
   end
 
   def update
-
+    @booking = Booking.find(params[:id])
+    @booking.update(booking_params)
+    redirect_to booking_path(@booking)
   end
 
-  def delete
-
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_path
   end
 
   private
