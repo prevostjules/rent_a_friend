dc6aad46a8f180bb2ff3198e3a8db3f18a5850e4
Jules Prevost <jprevostdev@gmail.com>
2020-06-16 17:25:23 +0200

    add sign in log out and working on sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:first_name, :last_name, :birth_date) }
+  end
 end
