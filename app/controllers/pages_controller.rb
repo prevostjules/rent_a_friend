c815b3688897331e8dfeed6c139895892661aafa
AntoineBarck <antoine.barck@gmail.com>
2020-06-16 15:00:08 +0200

    part1 - home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,18 @@
 class PagesController < ApplicationController
+
+
   def home
+    @cities = City.all
+    # if params[:city]
+    #   @friends = Friend.search(params[:city])
+    # else
+    #   @friends = Friend.all
+    # end
   end
 end
+
+# var = toutes les villes enregistres
+
+# un form dans la home avevc menu deroulant sur ville preenregistre
+
+# f.association
