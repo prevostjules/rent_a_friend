b646b27b96b1d5c63584a6f0b294d04f35edeca0
matthieu hedin <hedin.matthieu75@gmail.com>
2020-06-18 16:10:41 +0200

    map

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,6 +1,14 @@
 class FriendsController < ApplicationController
   def index
-      @friends = Friend.all
+      @friends = Friend.geocoded
+
+      @markers = @friends.map do |friend|
+        {
+          lat: friend.latitude,
+          lng: friend.longitude,
+          infoWindow: render_to_string(partial: "info_window", locals: { friend: friend }),
+        }
+      end
   end
 
   def show
